75e3d8308f4067df5f2a6f5cdf84526d977ffec4
Benjamin Rosario <benjaminfl119@gmail.com>
2025-08-17 19:06:59 -0400

    fixing

--- payroll/models.py
+++ payroll/models.py
@@ -661,7 +661,7 @@ class DeductionXuser(BaseModels):
         DeductionXuser.objects.bulk_create(
             [
                 DeductionXuser(
-                    id=len(DeductionXuser.object.all()) + 1,
+                    id=len(DeductionXuser.objects.all()) + 1,
                     user=user,
                     deduction=Deductions.objects.get(deduction_id=ded_id),
                     state="A",
